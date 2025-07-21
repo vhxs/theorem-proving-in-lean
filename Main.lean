d0cc024a5a0c7fb55cbe0fc129ab3d1ef3cbd367
Vikram Saraph <vikram.saraph.22@gmail.com>
2025-07-20 23:00:50 -0400

    restructure/rename

--- Main.lean
+++ Main.lean
@@ -1,4 +1,4 @@
 import TheoremProvingInLean
 
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  pure ()
--- TheoremProvingInLean.lean
+++ TheoremProvingInLean.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `TheoremProvingInLean` library.
 -- Import modules here that should be built as part of the library.
-import TheoremProvingInLean.Basic
+import TheoremProvingInLean.PropositionsAndProofs
