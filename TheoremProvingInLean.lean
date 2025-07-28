28c4c08efca8687ac27f3f8fadb72d0351030eb7
Vikram Saraph <vikram.saraph.22@gmail.com>
2025-07-28 18:12:20 -0400

    new section with one proof

--- TheoremProvingInLean.lean
+++ TheoremProvingInLean.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `TheoremProvingInLean` library.
 -- Import modules here that should be built as part of the library.
 import TheoremProvingInLean.PropositionsAndProofs
+import TheoremProvingInLean.QuantifiersAndEquality
---
+++ TheoremProvingInLean/QuantifiersAndEquality.lean
@@ -0,0 +1,15 @@
+-- Chapter 4 exercises
+-- https://leanprover.github.io/theorem_proving_in_lean4/Quantifiers-and-Equality/#Theorem-Proving-in-Lean-4--Quantifiers-and-Equality--Exercises
+
+variable {α : Type} (p q : α → Prop)
+
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
+Iff.intro
+  (fun haxpxqx =>
+    And.intro
+      (fun x => And.left (haxpxqx x))
+      (fun x => And.right (haxpxqx x))
+  )
+  (fun ⟨ haxpx, haxqx ⟩ =>
+    (fun x => And.intro (haxpx x) (haxqx x))
+  )
